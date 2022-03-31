c9402e6f6698ee31f62326e8cdfbd3b4d394177f
rrrs21 <rrrs21@gmail.com>
2022-03-30 23:28:20 -0300

    Commit class livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -2,12 +2,66 @@ package br.com.dio;
 
 import br.com.dio.model.Gato;
 
+import java.util.Objects;
+
 public class PrimeiroPrograma {
     public static void main(String[] args) {
                          /*int a = 5;
         int b = 3;
                       System.out.println("Helo World"+ (a+b));*/
         Gato gato = new Gato("Mel", "Rosa", 5);
+        Livro livro = new Livro("O senhor dos aneis",500);
         System.out.println(gato);
+        System.out.println(livro);
+    }
+}
+
+class Livro{
+    private String nome;
+    private Integer numPaginas;
+
+    public Livro() {
+    }
+
+    public Livro(String nome, Integer numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Livro livro = (Livro) o;
+        return Objects.equals(nome, livro.nome) && Objects.equals(numPaginas, livro.numPaginas);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, numPaginas);
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
     }
 }
