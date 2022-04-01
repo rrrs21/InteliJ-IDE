34737e9f02aec3ef3282220c557e5f0474c9fca6
rrrs21 <rrrs21@gmail.com>
2022-03-31 22:02:55 -0300

    Inserindo a classe com exemplo de casting. Ou seja, com o exemplo de conversão de tipos de valores.

---
+++ src/br/com/dio/Casting.java
@@ -0,0 +1,10 @@
+package br.com.dio;
+
+public class Casting {
+    public static void main(String[] args) {
+        int b = 1;
+        long c = 1000000;
+        b=(int) c; //conversão de tipos de variáriveis. Pode ser utilizado com qualquer tipo de dados.
+
+    }
+}
